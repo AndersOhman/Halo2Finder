a582aab889ca026729398d2574d11a9a6ee0a77f
Anders Ohman <anders_ohman@brown.edu>
2020-06-03 20:14:28 -0400

    Reports game statistics
    Script now reports the percentage of games you played together, as well as the date & time of your first and last games (both ever, and together).

--- Halo2Finder.jl
+++ Halo2Finder.jl
@@ -26,21 +26,40 @@
 
 ===============================================================================
 #
-# Program:                  The Halo2 Online Friend Finder
+# Program:                  Halo2Finder.jl
+# Name:                     The Halo2 Online Friend Finder
 # Purpose:                  To see if/when you played Halo2 on XBL with someone
 # Description:                  Dedicated to Aaron Crootof
 # Created by: 				Anders Ohman (mjolnirman@gmail.com)
 # Created on: 				2018-03-21
-# Updated on:               2020-05-19
+# Updated on:               2020-06-03
 #
 ==============================================================================#
 
 # Libraries (The Library)
 using Cascadia
 using Gumbo
+using OrderedCollections
 # Ask user for their GT and friend GT ()
 println("**********************************************************")
-# NOTE: Pop in some tiny ASCII here
+println("                   nmmmmmmmmmmmmmmm                       ")
+println("                 MMMMMMMMMMMMMMMMMMMMMMMM8                ")
+println("            MMMMMMMM^^^^^^^^^^^^^^MMMMMMMMMMMM            ")
+println("         MMMMMM~         ,,,,,       MMMMMMMMMMMM         ")
+println("       MMMM          /MMMMMMMMMMM       MMMMMMMMMM        ")
+println("     MMMM          //MM       MMMMM        MMMMMMMM       ")
+println("    MMM         ```            MMMM        MMMMMMMM       ")
+println("MMMMMM                         MMMMM         MMMMMMM:     ")
+println(" MMMMM                        MMMMM           MMMMMMMMMIII")
+println("                           MMM               MMMMMMMM,    ")
+println("                     /MMMMM         //       MMMMMMM      ")
+println("                  /MMMMM   ~MMMMMMMMM      MMMMMMMM       ")
+println("              /MMMMMMMMMMMMMMMMMMM/      MMMMMMMMM        ")
+println("                                      MMMMMMMMMMM         ")
+println("             MMMMMMM           +MMMMMMMMMMMMMM:           ")
+println("              MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM              ")
+println("                     ^^^^^^^^^^^^^^^                      ")
+println("**********************************************************")
 println("Welcome to the Halo2 Friend Finder!")
 println("This will look through Bungie.net for old Halo2 game records.")
 print("> Please enter Gamertag 1: ") #D33pTh0ugh7
@@ -69,13 +88,16 @@ pages = parse(Int64, nodeText(qs[2]))
 #println("**********************************************************")
 println("Going through $pages pages to check $items games.")
 # Open each page to grab game IDs
+matchFound = false
 page = 1
 gameIDset = Set()
-gameDate = Dict()
-gamenum = 0
+gameDateDict = Dict()
+
 while page <= pages
     global page
     global gamenum
+    global matchFound
+    gamenum = 0
     #println("Searching page $page...")
     url = "https://halo.bungie.net/Stats/PlayerStatsHalo2.aspx?player=$gamertag1&ctl00_mainContent_bnetpgl_recentgamesChangePage=$page"
     # Get each game ID from page, push to a Set
@@ -83,15 +105,16 @@ while page <= pages
     for result_line in split(h, "\n")
         gameid = match(r"gameid=(\d+)&", result_line)
         if gameid != nothing
-            gamenum = gameid[1]
+            gamenum = parse(Int,gameid[1])
             push!(gameIDset, gamenum)
+            matchFound = true
             #println("Game # $gamenum")
         end
         # Grab the date of the game, push to a Dictionary
         dateid = match(r"(\d+\/{1}\d+\/\d+ \d+:\d+:\d+ [A-Z]{2})",result_line)
         if dateid != nothing && gamenum != 0
             daynum = dateid[1]
-            gameDate[gamenum] = daynum
+            gameDateDict[gamenum] = daynum
             #println("Date: $daynum")
         end
     end
@@ -109,7 +132,7 @@ totalHrs = round((totalMins/60);digits=2)
 println("This will take at least $totalSecs sec / $totalMins min / $totalHrs hrs.")
 #NOTE: Timenow and time to finish https://en.wikibooks.org/wiki/Introducing_Julia/Working_with_dates_and_times
 # Search each game for friend's name
-matchSet = Set()
+matchSet = OrderedSet()
 lastgame = 0
 for game in sort(collect(gameIDset))
     url = "https://halo.bungie.net/Stats/GameStatsHalo2.aspx?gameid=$game"
@@ -122,10 +145,10 @@ for game in sort(collect(gameIDset))
         if gtmatch != nothing
             if game != lastgame
                 push!(matchSet, game)
-                write(output_file, "Match #$(length(matchSet)): $game\n$url\n\n")
+                write(output_file, "Match #$(length(matchSet)): $game\n$url\n")
                 #println("\nMatch #$(length(matchSet)): $game\n$url")
                 lastgame = game
-                datetime = gameDate[game]
+                datetime = gameDateDict[game]
                 write(output_file, "Date: $datetime\n\n")
                 #println("$datetime\n\n")
                 print("*")
@@ -134,15 +157,50 @@ for game in sort(collect(gameIDset))
     end
     print(".")
 end
+
+# Percent of all games together:
+totalMatches = length(matchSet)
+percentGames = round(100*(totalMatches/length(gameIDset));digits=2)
+write(output_file, "$gamertag1 played $totalMatches of $items games ($percentGames%) with $gamertag2.\n\n")
+
+# First game ever:
+earliestGame = minimum(gameDateDict)
+earliestGameNum = earliestGame[1]
+earliestGameDate = earliestGame[2]
+write(output_file, "The earliest game you ever played was #$earliestGameNum on $earliestGameDate.\n")
+
+# Last game ever:
+latestGame = maximum(gameDateDict)
+latestGameNum = latestGame[1]
+latestGameDate = latestGame[2]
+write(output_file, "The latest game you ever played was #$latestGameNum on $latestGameDate.\n\n")
+
+# Last game together:
+lastGameTogether = pop!(matchSet)
+lastGameTogetherDate = gameDateDict[lastGameTogether]
+
+# First game together:
+println(length(matchSet))
+while length(matchSet) > 1
+    pop!(matchSet)
+end
+println(length(matchSet))
+firstGameTogether = pop!(matchSet)
+println(length(matchSet))
+firstGameTogetherDate = gameDateDict[firstGameTogether]
+
+write(output_file, "The earliest game you ever played TOGETHER was $firstGameTogether on $firstGameTogetherDate.\n")
+write(output_file, "The latest game you ever played TOGETHER was $lastGameTogether on $lastGameTogetherDate.\n\n")
+
 close(output_file)
 print("\n")
 println("**********************************************************")
 println("Finished scanning games.")
 #NOTE: Pop in actual time elapsed calculation
 # Report result summary
-if length(matchSet) > 0
-    println("$(length(matchSet)) matches found! Please check output file for a copy.\n")
-    #NOTE: Print out stats like % games, first game, etc.
+if matchFound
+    println("$totalMatches matches found! Please check output file for a copy.\n")
+
     data_file = open(file,"r")
     for line in eachline(data_file)
         println("$line")
--- README.md
+++ README.md
@@ -36,7 +36,7 @@ Dedicated to Aaron Crootof.
 - **March 21st, 2018:** Original Release
 - **May 19th, 2020:** It looks like everything about Julia changed since I wrote this! The [Requests](https://github.com/JuliaWeb/Requests.jl) package I used has been depreciated in favor of [HTTP](https://github.com/JuliaWeb/HTTP.jl). In addition, some basic Julia syntax (like `readline(STDIN)`) has changed as well, breaking... everything. It may take some time to retrofit this...
 - **May 20th, 2020:** Okay, that wasn't too bad. The old version should still work under [Julia 0.7](https://julialang.org/downloads/oldreleases/#v070_aug_8_2018), but the latest version has been updated to the standards of [Julia 1.0](https://julialang.org/downloads/). I also fixed the Guest bug (needed two escape `\` for a string + regex combo).
-- **May 27th, 2020:** Now the date and time of each game is grabbed and written to the output.
+- **June 3rd, 2020:** Calculates percentage of games played together, and reports date & time of first and last games. Used `OrderedCollections.jl` to do so.
 
 ### Core Concept
 To see if, when, and how many times you played [Halo 2](https://en.wikipedia.org/wiki/Halo_2) on [Xbox Live](https://www.xbox.com/en-US/live) with someone else, by searching the legacy Game Viewer for both of your gamertags and exporting a list of games (with links to the viewer pages).
@@ -57,26 +57,27 @@ To see if, when, and how many times you played [Halo 2](https://en.wikipedia.org
 - Internet access to connect to [Halo.Bungie.Net](https://halo.bungie.net/Stats/PlayerStatsHalo2.aspx).
 
 Julia Packages:
-- [Cascadia](https://github.com/Algocircle/Cascadia.jl)
-- [Gumbo](https://github.com/JuliaWeb/Gumbo.jl)
+- [Cascadia.jl](https://github.com/Algocircle/Cascadia.jl)
+- [Gumbo.jl](https://github.com/JuliaWeb/Gumbo.jl)
+- [OrderedCollections.jl](https://github.com/JuliaCollections/OrderedCollections.jl)
 
 ### Known Bugs
 - The `sleepTime` variable exists because the [Halo.Bungie.Net](https://halo.bungie.net/Stats/PlayerStatsHalo2.aspx) website will occasionally time out requests that are too rapid in succession. While the `0.5` setting (a half-second delay) was sufficient to resolve this for me, you may need to increase this value if you are seeing this issue frequently.
 
 ### TODO
 Next steps:
-- [X] Update to Julia 1.0 syntax changes
-- [X] Fix Guest searching
-- [ ] Tiny ASCII boot logo `(43)`
-- [X] Parse timestamp of each game
-- [X] Get date info for each game
+- [X] ~~Update to Julia 1.0 syntax changes~~
+- [X] ~~Fix Guest searching~~
+- [X] ~~Tiny ASCII boot logo~~
+- [X] ~~Parse timestamp of each game~~
+- [X] ~~Get date info for each game~~
 - [ ] Add a "verbose" toggle for the `.` and `*` display
-- [ ] Report additional game stats (i.e. % of games, first game, win/loss) `(131)`
+- [X] ~~Report additional game stats (i.e. % of games, first/last game,~~ win:loss)
 
 Time estimate things:
-- [ ] Grab the current time and give finish time estimate `(110)`
-- [ ] Implement a loading bar `(98)`
-- [ ] Report actual time elapsed `(141)`
+- [ ] Grab the current time and give finish time estimate `(133)`
+- [ ] Implement a loading bar `(121)`
+- [ ] Report actual time elapsed `(199)`
 
 ### Contact
 Anders Ohman\
